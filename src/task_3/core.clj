99091209a8b80ed3870bea730132d3992b164cdf
Крутиков Никита <krutikov.nikita7@gmail.com>
2015-12-28 11:30:56 +0600

    task_3 with 2% accuracy

--- src/task_3/core.clj
+++ src/task_3/core.clj
@@ -1,51 +1,75 @@
 (ns task-3.core)
 
-(defrecord GridValue [pos value])
-
-(def base (GridValue. 0 0))
+;(defn stepOfIntegration
+;  [func position step]
+;  (*
+;    (/
+;      (+
+;        (func (+ step position))
+;        (func position) )
+;      2)
+;    step))
+;
+;(defn createSequence [func position step]
+;  (lazy-seq
+;    (cons createSequence )))
+;
+;(defn inSeq [f, h]
+;  (lazy-seq (cons base (map (fn [a]
+;                              (let [b (assoc a :value (+ (integrationStep f (:pos a) h) (:value a)))]
+;                                (assoc b :pos (+ h (:pos b))))
+;                              )
+;                            (inSeq f h)))))
+;
+;(defn quadraticFunc
+;  "test quadratic function"
+;  [x]
+;  (* x x)
+;  )
+;
+;(defn createIntegrale [f, h]
+;  (let [seq (inSeq f h) ]
+;    (fn [a]
+;      (let [last (mod a h)]
+;        (+ (stepOfIntegration f (- a last) last) (:value (nth seq (int (quot a h))))))
+;      )
+;    )
+;  )
+;
+;
+;(defn integrate  [func step]
+;  (:pre [(> x 0)])
+;  (let [seq (inSeq func step)]
+;    (fn [x]
+;      (let [last (mod x step)]
+;        (+ (stepOfIntegration func (- x last) last) (:value (nth seq (int (quot x step)))))
+;      )
+;    )
+;  )
+;)
+;
+;(def qF (createIntegral quadraticFunc 1/10))
 
 (defn stepOfIntegration
-  [func position step]
+  [func prevValue position step]
   (*
     (/
       (+
-        (func (+ step position))
+        prevValue
         (func position) )
       2)
     step))
 
-(defn inSeq [f, h]
-  (lazy-seq (cons base (map (fn [a]
-                              (let [b (assoc a :value (+ (integrationStep f (:pos a) h) (:value a)))]
-                                (assoc b :pos (+ h (:pos b))))
-                              )
-                            (inSeq f h)))))
 
-(defn quadraticFunc
-  "test quadratic function"
-  [x]
-  (* x x)
-  )
-
-(defn createIntegrale [f, h]
-  (let [seq (inSeq f h) ]
-    (fn [a]
-      (let [last (mod a h)]
-        (+ (stepOfIntegration f (- a last) last) (:value (nth seq (int (quot a h))))))
-      )
-    )
-  )
-
-
-(defn integrate  [func step]
-  (:pre [(> x 0)])
-  (let [seq (inSeq func step)]
-    (fn [x]
-      (let [last (mod x step)]
-        (+ (stepOfIntegration func (- x last) last) (:value (nth seq (int (quot x step)))))
-      )
-    )
-  )
-)
-
-(def qF (createIntegral quadraticFunc 1/10))
+(defn integrate
+  [func step x]
+  (let [integrationSteps
+        (map
+          first
+          (iterate
+            (fn [[sum_fn f_n position]]
+              [(+ sum_fn  (stepOfIntegration func f_n position step))
+               ( func (+ position step))
+               (+ position step)])
+            [0 (func 0) 0]))]
+    (nth integrationSteps (int (quot x step)))))
--- src/task_3/test.clj
+++ src/task_3/test.clj
@@ -6,14 +6,14 @@
 (defn quad [x] (double (* x x)))
 
 (test/deftest step-test
-  (test/testing "comparison with analytic integral of x^2, accyracy 1%, value 0"
-    (test/is (<= (integrate quad 0.1 0) (*(quadAnIntegr 0) 1.01)))
-    (test/is (>= (integrate quad 0.1 0) (*(quadAnIntegr 0) 0.99))))
-  (test/testing "comparison with analytic integral of x^2, accyracy 1%, value 1"
-    (test/is (<= (integrate quad 0.1 1) (*(quadAnIntegr 1) 1.01)))
-    (test/is (>= (integrate quad 0.1 1) (*(quadAnIntegr 1) 0.99))))
-  (test/testing "comparison with analytic integral of x^2, accyracy 1%, value 10"
-    (test/is (<= (integrate quad 0.1 10) (*(quadAnIntegr 10) 1.01)))
-    (test/is (>= (integrate quad 0.1 10) (*(quadAnIntegr 10) 0.99)))))
+  (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 0"
+    (test/is (<= (integrate quad 0.01 0) (*(quadAnIntegr 0) 1.02)))
+    (test/is (>= (integrate quad 0.01 0) (*(quadAnIntegr 0) 0.98))))
+  (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 1"
+    (test/is (<= (integrate quad 0.01 1) (*(quadAnIntegr 1) 1.02)))
+    (test/is (>= (integrate quad 0.01 1) (*(quadAnIntegr 1) 0.98))))
+  (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 10"
+    (test/is (<= (integrate quad 0.01 10) (*(quadAnIntegr 10) 1.02)))
+    (test/is (>= (integrate quad 0.01 10) (*(quadAnIntegr 10) 0.98)))))
 
 (test/run-tests)
