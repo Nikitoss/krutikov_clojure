681b32b2cb646f6bc1360c8da05d11a46a4ad5a1
Крутиков Никита <krutikov.nikita7@gmail.com>
2015-12-28 11:45:43 +0600

    task_3 with 1% accuracy

--- src/task_3/test.clj
+++ src/task_3/test.clj
@@ -7,13 +7,13 @@
 
 (test/deftest step-test
   (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 0"
-    (test/is (<= (integrate quad 0.01 0) (*(quadAnIntegr 0) 1.02)))
-    (test/is (>= (integrate quad 0.01 0) (*(quadAnIntegr 0) 0.98))))
+    (test/is (<= (integrate quad 0.001 0) (*(quadAnIntegr 0) 1.01)))
+    (test/is (>= (integrate quad 0.001 0) (*(quadAnIntegr 0) 0.99))))
   (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 1"
-    (test/is (<= (integrate quad 0.01 1) (*(quadAnIntegr 1) 1.02)))
-    (test/is (>= (integrate quad 0.01 1) (*(quadAnIntegr 1) 0.98))))
+    (test/is (<= (integrate quad 0.001 1) (*(quadAnIntegr 1) 1.01)))
+    (test/is (>= (integrate quad 0.001 1) (*(quadAnIntegr 1) 0.99))))
   (test/testing "comparison with analytic integral of x^2, accyracy 2%, value 10"
-    (test/is (<= (integrate quad 0.01 10) (*(quadAnIntegr 10) 1.02)))
-    (test/is (>= (integrate quad 0.01 10) (*(quadAnIntegr 10) 0.98)))))
+    (test/is (<= (integrate quad 0.001 10) (*(quadAnIntegr 10) 1.01)))
+    (test/is (>= (integrate quad 0.001 10) (*(quadAnIntegr 10) 0.99)))))
 
 (test/run-tests)
